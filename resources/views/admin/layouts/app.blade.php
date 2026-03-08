acfb22eaffcdf6c078fe4d93380b5b5fc1f099e7
guinwogbegno koffi <koffi.guinwogbegno@ipnetinstitute.com>
2026-03-08 07:46:15 +0000

    second commit

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -47,19 +47,19 @@
                 <div class="navbar-brand-box horizontal-logo">
                     <a href="index.html" class="logo logo-dark">
                         <span class="logo-sm">
-                            <img src="{{ asset('admin/assets/images/logo_original.png') }}" alt="" height="22">
+                            <img src="{{ asset('admin/assets/images/logov2.jpeg') }}" alt="" height="22">
                         </span>
                         <span class="logo-lg">
-                            <img src="{{ asset('admin/assets/images/logo_original.png') }}" alt="" height="17">
+                            <img src="{{ asset('admin/assets/images/logov2.jpeg') }}" alt="" height="17">
                         </span>
                     </a>
 
                     <a href="index.html" class="logo logo-light">
                         <span class="logo-sm">
-                            <img src="{{ asset('admin/assets/images/logo_original.png') }}" alt="" height="22">
+                            <img src="{{ asset('admin/assets/images/logov2.jpeg') }}" alt="" height="22">
                         </span>
                         <span class="logo-lg">
-                            <img src="{{ asset('admin/assets/images/logo_original.png') }}" alt="" height="17">
+                            <img src="{{ asset('admin/assets/images/logov2.jpeg') }}" alt="" height="17">
                         </span>
                     </a>
                 </div>
@@ -175,19 +175,19 @@
                 <!-- Dark Logo-->
                 <a href="{{ route('dashboard') }}" class="logo logo-dark">
                     <span class="logo-sm">
-                        <img src="{{ asset('admin/assets/images/logo_original.png') }}" alt="" height="70">
+                        <img src="{{ asset('admin/assets/images/logov2.png') }}" alt="" height="70">
                     </span>
                     <span class="logo-lg">
-                        <img src="{{ asset('admin/assets/images/logo_original.png') }}" alt="" height="70">
+                        <img src="{{ asset('admin/assets/images/logov2.png') }}" alt="" height="70">
                     </span>
                 </a>
                 <!-- Light Logo-->
                 <a href="{{ route('dashboard') }}" class="logo logo-light">
                     <span class="logo-sm">
-                        <img src="{{ asset('admin/assets/images/logo_original.png') }}" alt="" height="70">
+                        <img src="{{ asset('admin/assets/images/logov2.png') }}" alt="" height="70">
                     </span>
                     <span class="logo-lg">
-                        <img src="{{ asset('admin/assets/images/logo_original.png') }}" alt="" height="70">
+                        <img src="{{ asset('admin/assets/images/logov2.png') }}" alt="" height="70">
                     </span>
                 </a>
                 <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover"
